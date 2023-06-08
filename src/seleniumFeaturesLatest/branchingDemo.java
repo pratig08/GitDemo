d7884ca71dfb5a743e3ea131df64308b5758f512
Pratibha <pratig08@gmail.com>
2023-06-08 14:58:15 +0530

    Commit message

---
+++ src/seleniumFeaturesLatest/branchingDemo.java
@@ -0,0 +1,14 @@
+package seleniumFeaturesLatest;
+
+public class branchingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Main project in India");
+		System.out.println("Main project city: Bengaluru");
+		
+
+	}
+
+}
