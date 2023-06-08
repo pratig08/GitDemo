91d23a32e1c243b11d8db71991bdbbe2f67368d3
Pratibha <pratig08@gmail.com>
2023-06-08 15:13:56 +0530

    Developbranchcommit

--- src/seleniumFeaturesLatest/branchingDemo.java
+++ src/seleniumFeaturesLatest/branchingDemo.java
@@ -8,7 +8,8 @@ public class branchingDemo {
 		System.out.println("Main project in India");
 		System.out.println("Main project city: Bengaluru");
 		
-
+        System.out.println("Developbranch one");
+        System.out.println("Developbranch two");
 	}
 
 }
